3ce0c5e5e8e59e9c01626ffcf690d20fe87788a4
erix2016 <erixcamps@gmail.com>
2020-06-11 15:27:55 -0400

    Create primero.py

---
+++ primero.py
@@ -0,0 +1,5 @@
+# Archivo de Python como prueba. Fue creado en la plataforma de GitHub.
+print('*' * 15, 'Hello World!', '*' * 15)
+a = int(input("Introduzca un primer número entero: "))
+b = int(input("Introduzca un primer segundo entero: "))
+print("El resultado de sumar %s y s% es:" % (a,b), a * b)
